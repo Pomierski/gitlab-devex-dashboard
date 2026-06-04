ec091bd98d3f06e699ca5340137dca930c99916e
Pomierski <pomierskipiotr@gmail.com>
2026-06-04 12:41:53 +0200

    feat: swtich adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: process.env.SITE_URL || 'http://localhost:4321',
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
